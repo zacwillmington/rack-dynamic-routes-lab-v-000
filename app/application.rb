f87fa3e5f97c48aade0247aa81ca0f8cbfdaa0c0
Zac Willmington <zac.willmington@gmail.com>
2018-04-28 19:27:26 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,16 +1,18 @@
 require "pry"
 
 class Application
+
+    def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    
+
     binding.pry
     if req.match(/items/)
         binding.pry
-
     else
         resp.write "Route not found"
         rep.status = 404
     end
     resp.finish
+    end
 end
